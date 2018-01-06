c8aa17d7ab178a5538e5752ba5548e52e4dc2bc6
MartinBG <martingeo95@gmail.com>
2018-01-06 23:05:46 +0200

    parse commands and parse error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+// https://doc.rust-lang.org/book/second-edition/ch12-03-improving-error-handling-and-modularity.html
+// this is for command creating from command line arguments and error handling
+
+extern crate MiniGit;
+
 fn main() {
-    println!("Hello, world!");
+	//let a: [String] = &[String::from("asd"), String::from("dasd"), String::from("dsasd")];
+	//print!("{:?}", a.len());
+    let args: Vec<String> = env::args().collect();
+    // println!("{:?}", args);
+    println!("{:?}",MiniGit::parse_command(&args).unwrap());
 }
